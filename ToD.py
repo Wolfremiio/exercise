5acc030a32a0b47333cbd8ca6311097c5c1f30ff
Wolfremiio <101276349+Wolfremiio@users.noreply.github.com>
2024-05-22 17:24:47 +0200

    Create class todo and styles

--- ToD.py
+++ ToD.py
@@ -4,7 +4,12 @@ from tkinter import ttk
 
 class todo:
     def __init__(self, root):
-        pass
+        self.root = root
+        self.root.tittle = "To-do-list"
+        self.root.geometry("650x410+300+150")
+
+        self.label = Label(self.root, text="To-Do-List-App", 
+        font="ariel, 25 bold", width= 10, bd=5,bg= '#6CAC7F', fg = 'black' )
 
 
 def main():
