8956eed8930089ca9455e9c936488782a3c3c084
Wolfremiio <101276349+Wolfremiio@users.noreply.github.com>
2024-05-22 18:17:22 +0200

    Make a imput of ToDolist App

--- ToD.py
+++ ToD.py
@@ -8,8 +8,45 @@ class todo:
         self.root.tittle = "To-do-list"
         self.root.geometry("650x410+300+150")
 
-        self.label = Label(self.root, text="To-Do-List-App", 
-        font="ariel, 25 bold", width= 10, bd=5,bg= '#6CAC7F', fg = 'black' )
+        self.label = Label(
+            self.root,
+            text="To-Do-List-App",
+            font="ariel, 25 bold",
+            width=10,
+            bd=5,
+            bg="#6CAC7F",
+            fg="white",
+        )
+        self.label.pack(side="top", fill=BOTH)
+
+        self.labe2 = Label(
+            self.root,
+            text="Add Task",
+            font="ariel, 18 bold",
+            width=10,
+            bd=5,
+            bg="#6CAC7F",
+            fg="white",
+        )
+        self.labe2.place(x=40, y=54)
+
+        self.labe3 = Label(
+            self.root,
+            text="Tasks",
+            font="ariel, 18 bold",
+            width=10,
+            bd=5,
+            bg="#6CAC7F",
+            fg="white",
+        )
+        self.labe3.place(x=320, y=54)
+
+        # Aquí empezaros como en cuadro de texto donde ingresaremos las tareas, utilizaré el widget de cuadro de texto en tkinter
+
+        self.main_text = Listbox(
+            self.root, height=9, bd=3, width=23, font="ariel, 20 italic bold"
+        )
+        self.main_text.place(x=280, y=100)
 
 
 def main():
